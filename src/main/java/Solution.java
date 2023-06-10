3c8977c918cfbb769e07ee84272ca365c9a1528e
alek <wait82985@gmail.com>
2023-06-10 13:45:49 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,38 @@
+import java.util.Arrays;
+
 public class Solution {
     public int maxValue(int n, int index, int maxSum) {
-        //TODO
-        return 0;
+        int leftover = maxSum - n;
+        if (leftover == 0) return 1;
+        int subtractor = 1;
+        int indexValue = 2;
+        leftover--;
+        int i = 1;
+        while (leftover >= 0) {
+            if (subtractor == n) {
+                indexValue = indexValue + leftover / n;
+                return indexValue;
+            }
+            indexValue++;
+            int leftIndex = Math.max(index - i, -1);
+            int rightIndex = Math.min(n, index + i);
+            i++;
+            if (leftIndex != -1 && rightIndex != n) {
+                subtractor = subtractor + 2;
+            } else if (leftIndex == -1 && rightIndex == n) {
+                subtractor = n;
+            } else if (leftIndex == -1 || rightIndex == n) {
+                subtractor++;
+            }
+
+            leftover = leftover - subtractor;
+            if (leftover < 0) {
+                indexValue--;
+                break;
+            }
+
+        }
+
+        return indexValue;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -19,4 +19,40 @@ public class SolutionTest {
         int output = 3;
         Assert.assertEquals(output, new Solution().maxValue(n, index, maxSum));
     }
+
+    @Test
+    public void maxValueTest3() {
+        int n = 3;
+        int index = 2;
+        int maxSum = 18;
+        int output = 7;
+        Assert.assertEquals(output, new Solution().maxValue(n, index, maxSum));
+    }
+
+    @Test
+    public void maxValueTest4() {
+        int n = 8;
+        int index = 7;
+        int maxSum = 14;
+        int output = 4;
+        Assert.assertEquals(output, new Solution().maxValue(n, index, maxSum));
+    }
+    @Test
+    public void maxValueTest5() {
+        int n = 7;
+        int index = 5;
+        int maxSum = 30;
+        int output = 6;
+        Assert.assertEquals(output, new Solution().maxValue(n, index, maxSum));
+    }
+
+
+    @Test
+    public void maxValueTest6() {
+        int n = 2;
+        int index = 0;
+        int maxSum = 798870804;
+        int output = 399435402;
+        Assert.assertEquals(output, new Solution().maxValue(n, index, maxSum));
+    }
 }
